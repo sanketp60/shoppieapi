ed4bc1633f2148399760b71c7cfa46db4e2f99e1
sanketp60 <spisat@hotmail.com>
2021-04-06 13:35:13 +0530

    Product Description is a TextField now, hence can be of multiple lines.

--- api/models.py
+++ api/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Product(models.Model):
     ProductID = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     Name = models.CharField(max_length=50)
-    Description = models.CharField(max_length=50)
+    Description = models.TextField()
     Category =  models.CharField(max_length=50, blank=False)
     UnitWeight  = models.FloatField()
     Picture = models.CharField(max_length=300)
-    
+
     def __str__(self):
         return str(self.Name)
