1a79277d21c595585a1fa5ecb4bc6df4882297a3
sanketp60 <spisat@hotmail.com>
2021-04-06 22:52:57 +0530

    Modularize URL patterns based on models

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
-urlpatterns = [
+
+product_urlpatterns = [
     path('product/', Product.as_view()),
     path('product/<str:productid>', Product.as_view()),
     path('product/search/<str:query>', Product.as_view(get_method_selector="search")),
     path('product/details/<str:productid>', Product.as_view(get_method_selector="details")),
 ]
+
+urlpatterns = product_urlpatterns
