47a044d714220175cbfe1f117e610f82bdd98988
sanketp60 <spisat@hotmail.com>
2021-04-29 23:02:14 +0530

    Minor fixes

--- shoppieapi/urls.py
+++ shoppieapi/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = 'ShoppieAPI admin dashboard'
+admin.site.site_header = 'ShoppieAPI manager dashboard'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
